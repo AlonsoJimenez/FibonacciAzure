de7c4021fafb79d4cc1a257a4053acca17b9385a
Alonso <alonso.jimenez@hotmail.com>
2020-07-29 16:24:49 -0600

    Modificacion html

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola mundo"
+    return render_template("calculator.html")
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port="8080", debug=True)
+
+
 
